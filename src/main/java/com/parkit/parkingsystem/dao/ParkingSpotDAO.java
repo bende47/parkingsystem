3ed5e63a5efbfb070a8597c1c546802539ec0483
bende47 <bendejustin@gmail.com>
2020-03-05 20:18:26 +0100

    Ajout de quelques methodes et optimisation du code

--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -1,59 +1,106 @@
 package com.parkit.parkingsystem.dao;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
-import com.parkit.parkingsystem.constants.DBConstants;
-import com.parkit.parkingsystem.constants.ParkingType;
-import com.parkit.parkingsystem.model.ParkingSpot;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.parkit.parkingsystem.config.DataBaseConfig;
+import com.parkit.parkingsystem.constants.DBConstants;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.model.ParkingSpot;
+
 public class ParkingSpotDAO {
-    private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
 
-    public DataBaseConfig dataBaseConfig = new DataBaseConfig();
+	/*
+	 * Logger
+	 */
+	private Logger logger = LogManager.getLogger("ParkingSpotDAO");
+	public void setLogger(Logger testlogger) {
+		this.logger = testlogger;
+	}
+	
+	
+	public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public int getNextAvailableSlot(ParkingType parkingType){
-        Connection con = null;
-        int result=-1;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
-            ps.setString(1, parkingType.toString());
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                result = rs.getInt(1);;
-            }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
-        }catch (Exception ex){
-            logger.error("Error fetching next available slot",ex);
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-        return result;
-    }
+	public int getNextAvailableSlot(ParkingType parkingType) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int result = -1;
 
-    public boolean updateParking(ParkingSpot parkingSpot){
-        //update the availability fo that parking slot
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
-            ps.setBoolean(1, parkingSpot.isAvailable());
-            ps.setInt(2, parkingSpot.getId());
-            int updateRowCount = ps.executeUpdate();
-            dataBaseConfig.closePreparedStatement(ps);
-            return (updateRowCount == 1);
-        }catch (Exception ex){
-            logger.error("Error updating parking info",ex);
-            return false;
-        }finally {
-            dataBaseConfig.closeConnection(con);
-        }
-    }
+		try {
+			con = dataBaseConfig.getConnection();
+			ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
+			ps.setString(1, parkingType.toString());
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+			dataBaseConfig.closeResultSet(rs);
+			dataBaseConfig.closePreparedStatement(ps);
+		} catch (Exception ex) {
+			logger.error("Error fetching next available slot", ex);
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+				logger.error("Error closing next available slot", e);
+			}
+			
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error closing next available slot", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error closing next available slot", e);
+			}
+			
+		}
+		return result;
+	}
+
+	public boolean updateParking(ParkingSpot parkingSpot) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = dataBaseConfig.getConnection();
+			ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
+			ps.setBoolean(1, parkingSpot.isItAvailable());
+			ps.setInt(2, parkingSpot.getId());
+			int updateRowCount = ps.executeUpdate();
+			dataBaseConfig.closePreparedStatement(ps);
+			return (updateRowCount == 1);
+		} catch (Exception ex) {
+			logger.error("Error updating parking info", ex);
+			return false;
+		} finally {
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error updateParking ", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error updateParking", e);
+			}
+			
+		}
+	}
+
+	
 
 }
--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -3,6 +3,7 @@ package com.parkit.parkingsystem.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
 import org.apache.logging.log4j.LogManager;
@@ -16,37 +17,57 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class TicketDAO {
 
-	private static final Logger logger = LogManager.getLogger("TicketDAO");
+	private Logger logger = LogManager.getLogger("TicketDAO");
 
 	public DataBaseConfig dataBaseConfig = new DataBaseConfig();
+	
+	Connection con = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
 
 	public boolean saveTicket(Ticket ticket) {
-		Connection con = null;
+		
 		try {
 			con = dataBaseConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement(DBConstants.SAVE_TICKET);
+			ps = con.prepareStatement(DBConstants.SAVE_TICKET);			
 			ps.setInt(1, ticket.getParkingSpot().getId());
 			ps.setString(2, ticket.getVehicleRegNumber());
 			ps.setDouble(3, ticket.getPrice());
 			ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
 			ps.setTimestamp(5, (ticket.getOutTime() == null) ? null : (new Timestamp(ticket.getOutTime().getTime())));
+			System.out.println("Ticket saved");
 			return ps.execute();
 		} catch (Exception ex) {
 			logger.error("Error fetching next available slot", ex);
+			// throw new Exception("Error fetching next available slot", ex);
 		} finally {
-			dataBaseConfig.closeConnection(con);
-			return false;
+					
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error close resource PreparedStatement", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error close resource connection", e);
+			}
+			
 		}
+		return false;
 	}
 
 	public Ticket getTicket(String vehicleRegNumber) {
-		Connection con = null;
+		
 		Ticket ticket = null;
 		try {
 			con = dataBaseConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement(DBConstants.GET_TICKET);
+			ps = con.prepareStatement(DBConstants.GET_TICKET);
 			ps.setString(1, vehicleRegNumber);
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			if (rs.next()) {
 				ticket = new Ticket();
 				ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)), false);
@@ -56,82 +77,149 @@ public class TicketDAO {
 				ticket.setPrice(rs.getDouble(3));
 				ticket.setInTime(rs.getTimestamp(4));
 				ticket.setOutTime(rs.getTimestamp(5));
+				System.out.println("Ticket get");
 			}
-			dataBaseConfig.closeResultSet(rs);
-			dataBaseConfig.closePreparedStatement(ps);
 		} catch (Exception ex) {
 			logger.error("Error fetching next available slot", ex);
 		} finally {
-			dataBaseConfig.closeConnection(con);
-			return ticket;
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+				logger.error("Error close resource resultser", e);
+			}
+			
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error close resource PreparedStatement", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error close resource connection", e);
+			}
+			
 		}
+		return ticket;
 	}
 
 	public boolean updateTicket(Ticket ticket) {
-		Connection con = null;
+		
 		try {
 			con = dataBaseConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
+			ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
 			ps.setDouble(1, ticket.getPrice());
 			ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
 			ps.setInt(3, ticket.getId());
 			ps.execute();
+			System.out.println("Ticket updated");
 			return true;
 		} catch (Exception ex) {
 			logger.error("Error saving ticket info", ex);
-		} finally {
-			dataBaseConfig.closeConnection(con);
+		} finally {					
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error close resource PreparedStatement", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error close resource connection", e);
+			}
 		}
 		return false;
 	}
 
-	public boolean recurringUser(String vehicleRegNumber) {
-		Connection con = null;
-		ResultSet rs = null;
-		boolean applyReduction = false;
-		int count = 0;
+	public boolean verifyVehicleRegnumber(String vehicleRegNumber) {
+		
+		boolean result = true;
 		try {
 			con = dataBaseConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement(DBConstants.USER_RECURRING);
+			ps = con.prepareStatement(DBConstants.GET_VEHICULE_);
 			ps.setString(1, vehicleRegNumber);
 			rs = ps.executeQuery();
-			while (rs.next()) {
-				count = rs.getInt(1);
-
+			if (rs.next()) {
+				result = false;
 			}
-			if (count >= 1) {
-				applyReduction = true;
-			}
-			dataBaseConfig.closeResultSet(rs);
-			dataBaseConfig.closePreparedStatement(ps);
 		} catch (Exception ex) {
-			logger.error("Error fetching next available slot", ex);
+			logger.error("Error fetching doublon registration numbers", ex);
 		} finally {
-			dataBaseConfig.closeConnection(con);
-			return applyReduction;
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+				logger.error("Error close resource resultser", e);
+			}
+			
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error close resource PreparedStatement", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error close resource connection", e);
+			}
+			
 		}
+		return result;
 	}
 
-	public int countUser(String vehicleRegNumber) {
-		Connection con = null;
-		ResultSet rs = null;
-		int count = 0;
+	public boolean applyReduction5p(Ticket ticket) throws ClassNotFoundException, SQLException {
+		
+		int cpt = 0;
 		try {
 			con = dataBaseConfig.getConnection();
-			PreparedStatement ps = con.prepareStatement(DBConstants.USER_RECURRING);
-			ps.setString(1, vehicleRegNumber);
+			ps = con.prepareStatement(DBConstants.GET_VEHICULE_REGNUMBER);
+			ps.setString(1, ticket.getVehicleRegNumber());
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				count = rs.getInt(1);
-
+				cpt ++;
 			}
-			dataBaseConfig.closeResultSet(rs);
-			dataBaseConfig.closePreparedStatement(ps);
 		} catch (Exception ex) {
-			logger.error("Error fetching next available slot", ex);
+			logger.error("Error fetching reduction5percent", ex);
 		} finally {
-			dataBaseConfig.closeConnection(con);
-			return count;
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+				logger.error("Error close resource resultser", e);
+			}
+			
+			try {
+				if (ps != null)
+					ps.close();
+			} catch (Exception e) {
+				logger.error("Error close resource PreparedStatement", e);
+			}
+			
+			try {
+				if (con != null)
+					con.close();
+			} catch (Exception e) {
+				logger.error("Error close resource connection", e);
+			}
+			
 		}
+		if (cpt > 1)
+			return true;
+		else
+			return false;
+	}
+
+	public void setLogger(Logger testlogger) {
+		this.logger = testlogger;
 	}
 }
