f046922e589478eaafa4cfb8346bc80701451b70
bende47 <bendejustin@gmail.com>
2020-03-05 20:20:55 +0100

    Calcul de prix de parking

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,41 +1,40 @@
 package com.parkit.parkingsystem.service;
 
+import java.sql.SQLException;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-	public void calculateFare(Ticket ticket, boolean applyReduction) {
+	public void calculateFare(Ticket ticket, Boolean availableReduction) throws ClassNotFoundException, SQLException {
 		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
 			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 		}
 
-		double inHour = ticket.getInTime().getTime();
-		double outHour = ticket.getOutTime().getTime();
+		long intime = ticket.getInTime().getTime();
+		long outtime = ticket.getOutTime().getTime();
 
-		double duration = (outHour - inHour) / (1000 * 60 * 60);
-
-		if (duration <= 0.5) {
-			ticket.setPrice(0);
-			return;
+		float duration = (float) ((outtime - intime) / 3600000.00);
+		if (duration > 0.5) {
+			switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR: {
+				ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+				break;
+			}
+			case BIKE: {
+				ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unkown Parking Type");
+			}
+			if (availableReduction == true) {
+				ticket.setPrice(ticket.getPrice() * 0.95);
+			}
+		} else {
+			ticket.setPrice(0.00);
 		}
-
-		double reduction = applyReduction ? 0.95 : 1;
-
-		double fare;
-
-		switch (ticket.getParkingSpot().getParkingType()) {
-		case CAR:
-			fare = Fare.CAR_RATE_PER_HOUR;
-			break;
-
-		case BIKE:
-			fare = Fare.BIKE_RATE_PER_HOUR;
-			break;
-
-		default:
-			throw new IllegalArgumentException("Unkown Parking Type");
-		}
-		ticket.setPrice(reduction * duration * fare);
 	}
+
 }
