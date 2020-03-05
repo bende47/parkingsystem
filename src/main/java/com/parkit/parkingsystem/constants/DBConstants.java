2a4a6ce155d0c14fdfafa07b8ed01f7d17956210
bende47 <bendejustin@gmail.com>
2020-03-05 19:49:55 +0100

    ajout de constantes

--- src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
+++ src/main/java/com/parkit/parkingsystem/constants/DBConstants.java
@@ -8,6 +8,9 @@ public class DBConstants {
 	public static final String SAVE_TICKET = "insert into ticket(PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME) values(?,?,?,?,?)";
 	public static final String UPDATE_TICKET = "update ticket set PRICE=?, OUT_TIME=? where ID=?";
 	public static final String GET_TICKET = "select t.PARKING_NUMBER, t.ID, t.PRICE, t.IN_TIME, t.OUT_TIME, p.TYPE from ticket t,parking p where p.parking_number = t.parking_number and t.VEHICLE_REG_NUMBER=? order by t.OUT_TIME  limit 1";
+	
 	public static final String USER_RECURRING = "SELECT COUNT(VEHICLE_REG_NUMBER)  FROM ticket WHERE VEHICLE_REG_NUMBER = ?";
+    public static final String GET_VEHICULE_ = "SELECT TICKET.VEHICLE_REG_NUMBER FROM TICKET where VEHICLE_REG_NUMBER = ? AND OUT_TIME IS NULL";
+    public static final String GET_VEHICULE_REGNUMBER = "SELECT t.VEHICLE_REG_NUMBER FROM ticket t where t.VEHICLE_REG_NUMBER = ?";
 
 }
