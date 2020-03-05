d21fb3f9d6d794989415adfae4ba29eed678567a
bende47 <bendejustin@gmail.com>
2020-03-05 21:38:29 +0100

    DataBase Prepare Service

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,21 +1,16 @@
 package com.parkit.parkingsystem.integration;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -24,19 +19,26 @@ import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
+import java.sql.SQLException;
+
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
+
 
 	private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
 	private static ParkingSpotDAO parkingSpotDAO;
 	private static TicketDAO ticketDAO;
 	private static DataBasePrepareService dataBasePrepareService;
+	private static String regNumberString;
+	private static Ticket ticket;
+
 
 	@Mock
 	private static InputReaderUtil inputReaderUtil;
 
+
 	@BeforeAll
-	private static void setUp() throws Exception {
+	private static void setUp() {
 		parkingSpotDAO = new ParkingSpotDAO();
 		parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
 		ticketDAO = new TicketDAO();
@@ -44,92 +46,63 @@ public class ParkingDataBaseIT {
 		dataBasePrepareService = new DataBasePrepareService();
 	}
 
-	@AfterAll
-	private static void tearDown() {
-
-	}
-
-	@Test
-	@DisplayName("test Parking Car")
-	public void testParkingACar() throws Exception {
+	@BeforeEach
+	private void setUpPerTest() throws Exception {
 		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(regNumberString = "ABCDEF");
 		dataBasePrepareService.clearDataBaseEntries();
-		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingService.processIncomingVehicle();
-		Ticket getTicketTest = ticketDAO.getTicket("ABCDEF");
-		assertThat(getTicketTest.getVehicleRegNumber()).isEqualTo("ABCDEF");
-		assertThat(getTicketTest.getParkingSpot().isAvailable()).isEqualTo(false);
-
 	}
 
+	@AfterEach
+	private void tearDown() {
+		dataBasePrepareService.clearDataBaseEntries();
+	}
+
+
 	@Test
-	@DisplayName("test Parking Exit Car 1h")
+	public void testParkingACar() {
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		// int parkingPlace = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
+		parkingService.processIncomingVehicle();
+		ticket = ticketDAO.getTicket(regNumberString);
+		System.out.println(ticketDAO.getTicket(regNumberString));
+		assertEquals(ticket.getVehicleRegNumber(), regNumberString);
+		assertEquals(ticket.getParkingSpot().isItAvailable(), false);
+	}
+
+
+	@Test
 	public void testParkingLotExit() throws Exception {
-		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEFG");
-		dataBasePrepareService.clearDataBaseEntries();
-
 		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(regNumberString);
 		parkingService.processIncomingVehicle();
+		ticket = ticketDAO.getTicket(regNumberString);
+		Thread.sleep(1000);
+		parkingService.processExitingVehicle();
+		Ticket ticket2 = ticketDAO.getTicket(regNumberString);
 
-		Ticket TicketTest = ticketDAO.getTicket("ABCDEFG");
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-		Date outTime = sdf.parse("2020/03/24 10:30");
-
-		TicketTest.setOutTime(outTime);
-		TicketTest.setPrice(1.5);
-
-		ticketDAO.updateTicket(TicketTest);
-
-		Date dateExit = TicketTest.getOutTime();
-
-		double priceExpected = Fare.CAR_RATE_PER_HOUR * 1;
-
-		double price = TicketTest.getPrice();
-
-		assertThat(priceExpected).isEqualTo(price);
-		assertThat(outTime).isEqualTo(dateExit);
-
+		assertNotNull(ticket2.getPrice());
+		assertNotNull(ticket2.getOutTime());
+		assertEquals(ticket2.getInTime(), ticket.getInTime());
+		assertEquals(ticket2.getVehicleRegNumber(), ticket.getVehicleRegNumber());
 	}
 
 	@Test
-	@DisplayName("Vehicle Already Enter")
-	public void VehicleAlreadyEnterTest() throws Exception {
-		when(inputReaderUtil.readSelection()).thenReturn(1);
-		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEFH");
-		dataBasePrepareService.clearDataBaseEntries();
-		ParkingService parkingServiceIn1 = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingServiceIn1.processIncomingVehicle();
+	public void testRecurrentVehicleReductionAvailable() throws Exception {
+		//ARRANGE
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn(regNumberString);
 
-		ParkingService parkingServiceIn2 = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-		parkingServiceIn2.processIncomingVehicle();
+		//ACT
+		parkingService.processIncomingVehicle();
+		Thread.sleep(1000);
+		parkingService.processExitingVehicle();
+		parkingService.processIncomingVehicle();
+		Ticket ticket2 = ticketDAO.getTicket(regNumberString);
+		boolean test = ticketDAO.applyReduction5p(ticket2);
 
-		Ticket ticket2 = ticketDAO.getTicket("ABCDEFH");
-
-		boolean allowEnter = ParkingService.vehiculeAlreadyEnter(ticket2);
-
-		assertThat(allowEnter).isEqualTo(true);
-
-	}
-
-	@Test
-	@DisplayName("Vehicule Is Allow Enter")
-	public void vehiculeIsEnterTest() throws ParseException {
-		Ticket ticket1 = new Ticket();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-		Date outTime = sdf.parse("2020/03/24 10:30");
-
-		ticket1.setVehicleRegNumber("AZERTY");
-		ticket1.setOutTime(outTime);
-
-		Ticket ticket2 = new Ticket();
-
-		ticket2.setVehicleRegNumber("AZERTY");
-
-		boolean allowEnter = ParkingService.vehiculeAlreadyEnter(ticket2);
-
-		assertThat(allowEnter).isEqualTo(true);
+		//ASSERT
+		assertTrue(test);
 	}
 
 }
